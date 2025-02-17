6661622bfbe643c17f7f95b7c747e01686adae88
Pufikas <dominykas.guzys@gmail.com>
2025-02-17 22:47:07 +0200

    u2

--- 2011/u2/main.cpp
+++ 2011/u2/main.cpp
@@ -11,12 +11,73 @@ struct Pair {
 };
 
 void load(const char* path, Pair data[], int& n, int& k);
+void save(Pair data[], int& n);
+void sort(Pair data[], int& n, int& k);
+void calculate(Pair data[], int& n, int& k);
 
 int main() {
     Pair data[100];
     int n, k; // pairs, voters
 
     load("u2.txt", data, n, k);
+    calculate(data, n, k);
+    sort(data, n, k);
+    save(data, n);
+}
+
+
+void save(Pair data[], int& n) {
+    ofstream fout("u2res.txt");
+
+    for (int i = 0; i < n; i++) {
+        fout << data[i].name << " " << data[i].score << endl;
+    }
+
+    fout.close();
+}
+
+void sort(Pair data[], int& n, int& k) {
+    Pair temp;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (data[i].score < data[j].score) {
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
+}
+
+
+void calculate(Pair data[], int& n, int& k) {
+    for (int i = 0; i < n; i++) {
+        int tMin = INT_MAX, tMax = INT_MIN;
+        int aMin = INT_MAX, aMax = INT_MIN;
+        int tSum = 0, aSum = 0;
+
+        // technika
+        for (int x = 0; x < k; x++) {
+            int score = data[i].points[0][x];
+            if (score < tMin) tMin = score;
+            if (score > tMax) tMax = score;
+            tSum += score;
+        }
+
+        // artiskumas
+        for (int y = 0; y < k; y++) {
+            int score = data[i].points[1][y];
+            if (score < aMin) aMin = score;
+            if (score > aMax) aMax = score;
+            aSum += score;
+        }
+
+        int tRes = tSum - tMin - tMax;
+        int aRes = aSum - aMin - aMax;
+
+        data[i].score = tRes + aRes;
+    }
 }
 
 void load(const char* path, Pair data[], int& n, int& k) {
@@ -31,11 +92,8 @@ void load(const char* path, Pair data[], int& n, int& k) {
         for (int j = 0; j < 2; j++) {
             for (int x = 0; x < k; x++) {
                 fin >> data[i].points[j][x];
-                cout << data[i].points[j][x] << " ";
             }
         }
     }
-
     fin.close();
-
 }
