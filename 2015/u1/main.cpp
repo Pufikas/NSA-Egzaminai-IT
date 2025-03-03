9aed815692a179124599771c219adcdbadab0d72
Pufikas <dominykas.guzys@gmail.com>
2025-03-03 21:53:36 +0200

    u1

--- 2015/u1/main.cpp
+++ 2015/u1/main.cpp
@@ -1,34 +1,55 @@
 #include <fstream>
-#include <iostream>
-
 using namespace std;
 
 #define MAX_MER 10
 #define MAX_MOK 20
+#define MAX_DISH_COUNT 10
 
-
-void load(const char* path, int mer, int mok, int Mokinys[MAX_MOK]);
-
+void load(const char* path, int Mokinys[MAX_MOK], int DISH[MAX_MER]);
+void calculate(int Mokinys[MAX_MOK], int DISH[MAX_MER]);
+void save(const char* path, int Mokinys[MAX_MOK]);
 
 int main() {
-    int Mokinys[MAX_MOK];
+    int Mokinys[MAX_MOK] = {0};
+    int DISH[MAX_DISH_COUNT];
 
-    load("u1.txt", MAX_MER, MAX_MOK, Mokinys);
-    for (int i = 0; i < MAX_MOK; i++) {
-        cout << Mokinys[i] << " ";
-    }
+    load("u1.txt", Mokinys, DISH);
+    calculate(Mokinys, DISH);
+    save("u1res.txt", Mokinys);
 }
 
-void load(const char* path, int mer, int mok, int Mokinys[MAX_MOK]) {
+void calculate(int Mokinys[MAX_MOK], int DISH[MAX_MER]) {
+    for (int i = 0; i < MAX_MER; i++) {
+        int student = i + 1;
+        while (DISH[i] > 0 && i < MAX_MOK) {
+            Mokinys[student]++; // eats one dish
+            DISH[i]--; // - dish counter
+            student++; // move to next student
+        }
+    }       
+}
+
+void save(const char* path, int Mokinys[MAX_MOK]) {
+    ofstream fout(path);
+
+    for (int i = 0; i < MAX_MOK; i++) {
+        fout << Mokinys[i] << " ";
+    }
+
+    fout.close();
+}
+
+void load(const char* path, int Mokinys[MAX_MOK], int DISH[MAX_MER]) {
     fstream fin(path);
 
-    for (int i = 0; i < mer; i++) {
+    for (int i = 0; i < MAX_MER; i++) {
         fin >> Mokinys[i];
 
-        for (int j = mer; j < MAX_MOK; j++) {
-            Mokinys[j] = 0;
-        }
-        
+        DISH[i] = MAX_DISH_COUNT - Mokinys[i]; 
+    }
+
+    for (int i = MAX_MER; i < MAX_MOK; i++) {
+        Mokinys[i] = 0;
     }
 
     fin.close();
