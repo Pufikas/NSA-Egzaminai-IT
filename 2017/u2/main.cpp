f20a02a48724415a0ac9b79b56becc52c5d06da8
Pufikas <dominykas.guzys@gmail.com>
2025-03-13 21:51:40 +0200

    u2

---
+++ 2017/u2/main.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+
+int main() {
+    int n, x, y, dx, dy, r, g ,b;
+
+    return 0;
+}
+
+void load() {
+    fstream fin("u2.txt");
+
+    fin >> n;
+
+    for (int i = 0; i < n; i++) {
+        fin >> x >> y >> dx >> dy >> r >> g >> b;
+    }
+
+    fin.close();
+}
+
+void save() {
+    ofstream fout("u2res.txt");
+
+    fout.close();
+}
