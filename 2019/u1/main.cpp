587d4641d94cfb0887cb6b50d73d8f16c13a3aa6
Pufikas <dominykas.guzys@gmail.com>
2025-03-18 21:46:09 +0200

    u1wip

--- 2019/u1/main.cpp
+++ 2019/u1/main.cpp
@@ -12,7 +12,7 @@ void skaityti(int& n1, int& n3, int& n5, int& k, int& kn1, int& kn3, int& kn5, i
     fin.close();
 }
 
-void skaiciuoti(int& n1, int& n3, int& n5, int& k, int& ip1, int& ip3, int& ip5, int& liko, int& likn5, int& likn3, int& lik1) {
+void skaiciuoti(int& n1, int& n3, int& n5, int& k, int& ip1, int& ip3, int& ip5, int& liko, int& likn5, int& likn3, int& likn1) {
     ip5 = min(n5, k/5); // ipilta i 5l inda
     k -= ip5 * 5; // kiek liko aliejaus po ipilimo
 
@@ -32,13 +32,38 @@ void skaiciuoti(int& n1, int& n3, int& n5, int& k, int& ip1, int& ip3, int& ip5,
 
 }
 
+
+void papildomiIndai(int liko, int& papn1, int& papn3, int& papn5) {
+    papn5 = liko / 5;
+    liko -= papn5 * 5;
+
+    papn3 = liko / 3;
+    liko -= papn3 * 3;
+
+    papn1 = liko;
+}
+
+
+void rasyti(int ip1, int ip3, int ip5, int liko, int likn1, int likn3, int likn5, int papn1, int papn3, int papn5) {
+    ofstream fout("u1res.txt");
+
+    fout << ip1 << " " << ip3 << " " << ip5 << " " << liko << endl;
+    fout << likn1 << " " << likn3 << " " << likn5 << endl;
+    fout << papn1 << " " << papn3 << " " << papn5 << endl;
+
+    fout.close();
+}
+
 int main() {
     int n1, n3, n5, k, kn1, kn3, kn5, isl;
-    int ip5, ip3, ip1, liko;
-    int lik5, lik3, lik1;
+    int ip1 = 0, ip3 = 0, ip5 = 0, liko = 0;
+    int likn1 = 0, likn3 = 0, likn5 = 0;
+    int papn1 = 0, papn3 = 0, papn5 = 0;
 
     skaityti(n1, n3, n5, k, kn1, kn3, kn5, isl);
-    skaiciuoti(n1, n3, n5, k, ip1, ip3, ip5, liko, likn5, likn3, likn1);
+    skaiciuoti(n1, n3, n5, k, ip1, ip3, ip5, liko, likn1, likn3, likn5);
+    papildomiIndai(liko, papn1, papn3, papn5);
+    rasyti(ip1, ip3, ip5, liko, likn1, likn3, likn5, papn1, papn3, papn5);
 
     return 0;
-}
+}   
