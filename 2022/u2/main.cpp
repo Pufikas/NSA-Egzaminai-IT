b35845cd9b7619462b5f5603b85c947ca9f5ae1c
Pufikas <dominykas.guzys@gmail.com>
2025-04-05 23:29:36 +0300

    u1w

--- 2022/u2/main.cpp
+++ 2022/u2/main.cpp
@@ -3,14 +3,39 @@
 
 using namespace std;
 
-int Pratimas {
+struct Pratimas {
     int k; // pratimu kiekis
-    string pav;
-    string laikPav;
-    int atl; // atliktu pratimu kiekis
+    string pav[8];
+    string laikas[8];
+    int atl[8]; // atliktu pratimu kiekis
 };
 
-void skaityti(Pratimas P[]) {
+struct Dienos {
+    int pratPav[8];
+    int diena[8];
+    int uzgaista[8];
+    int suma[8];
+};
+
+void grupuoti() {
+    for (int i = 0; i < d; i++) {
+        bool rastas = false;
+        for (int j = 0; j < P[i].k; j++) {
+            if (!rastas) {
+                D[i].pratPav[j] = P[i].pratPav[j];
+                D[i].diena[j] = P[i].diena[j];
+                D[i].diena[j] = P[i].diena[j];
+
+            }
+
+            if ((D[i].pratPav[j] && D[i].diena[j]) != (P[i].pratPav[j] && P[i].diena[j])) {
+
+            }
+        }
+    }
+}
+
+void skaityti(int &d, Pratimas P[]) {
     ifstream fin("u2.txt");
 
     fin >> d;
@@ -20,21 +45,33 @@ void skaityti(Pratimas P[]) {
         for (int j = 0; j < P[i].k; j++) {
             fin >> ws;
             fin.get(laikinas, 15);
-            P[i].pav = laikinas;
+            P[i].pav[j] = laikinas;
 
             fin >> ws;
             fin.get(laikinas, 8);
-            P[i].laikPav = laikinas;
+            P[i].laikas[j] = laikinas;
 
-            fin >> P[i].atl;
+            fin >> P[i].atl[j];
         }
     }
 
     fin.close();
 }
 
+
+
 int main() {
     int d;
+    Pratimas P[30];
+
+    skaityti(d, P);
+
+    for (int i = 0; i < d; i++) {
+        for (int j = 0; j < P[i].k; j++) {
+            cout << P[i].k << " " << P[i].pav[j] << " " << P[i].laikas[j] << " " << P[i].atl[j] << endl;
+
+        }
+    }
 
     return 0;
 }
