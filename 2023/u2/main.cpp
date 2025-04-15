0aa95d229b3752da5b021bb90ca7a405000991ea
Pufikas <dominykas.guzys@gmail.com>
2025-04-15 21:51:51 +0300

    u2iwp:

--- 2023/u2/main.cpp
+++ 2023/u2/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,6 +13,11 @@ struct Slaptazodis {
     int skaiciuKiekis;
     int specSimboliai;
     string stiprumas;
+};
+
+struct PanasusSlap {
+    string name = "";
+    int panasumas = 0;
 };
 
 void skaityti(Slaptazodis S[], Slaptazodis V[], int &n, int &s) {
@@ -43,16 +49,53 @@ void skaityti(Slaptazodis S[], Slaptazodis V[], int &n, int &s) {
     fin.close();
 }
 
+void skaiciuoti(Slaptazodis S[], Slaptazodis V[], PanasusSlap PS[], int &n, int &s, int &cc) {
+    for (int i = 0; i < n; i++) {
+        int panasumoReiksme = 99;
+        int slapVt = -1; // slaptazodzio kuris labiausiai panasus vieta
+        for (int j = 0; j < s; j++) {
+            int pan = 0;
+
+            pan += abs(S[i].ilgis - V[j].ilgis);
+            pan += abs(S[i].didzRaides - V[j].didzRaides);
+            pan += abs(S[i].mazRaides - V[j].mazRaides);
+            pan += abs(S[i].skaiciuKiekis - V[j].skaiciuKiekis);
+            pan += abs(S[i].specSimboliai - V[j].specSimboliai);
+
+            PS[cc].slap = V[j].slap;
+            PS[cc].panasumas = pan;
+            cc++;
+        }
+    }
+}
+
+void rusiuoti(PanasusSlap PS[], int &cc) {
+    for (int i = 0; i < cc-1; i++) {
+        for (int j = i+1; j < cc; j++) {
+            if (PS[j].panasumas < PS[i].panasumas) {
+                swap(PS[i], PS[j]);
+            }
+        }
+    }
+}
+
+void rasyti() {
+    //
+}
+
 int main() {
     Slaptazodis S[5];
     Slaptazodis V[25];
+    PanasusSlap PS[25];
     int n, s;
 
     skaityti(S, V, n, s);
-
+    skaiciuoti(S, V, PS, n, s, cc);
+    rusiuoti(PS, cc);
  
     for (int i = 0; i < s; i++) {
-        cout << V[i].slap << " " << V[i].ilgis << " " << V[i].didzRaides << " " << V[i].mazRaides << " " << V[i].skaiciuKiekis << " " << V[i].specSimboliai << endl;
+        cout << ps[i] << endl;
+        //cout << V[i].slap << " " << V[i].ilgis << " " << V[i].didzRaides << " " << V[i].mazRaides << " " << V[i].skaiciuKiekis << " " << V[i].specSimboliai << endl;
         //cout << S[i].slap << endl;
         //cout << S[i].slap << " " 
         //     << S[i].ilgis << " " << S[i].mazRaides << " "
