2c30e524eb29cfe0440a4aa89daf07c7c43d4e53
Pufikas <dominykas.guzys@gmail.com>
2025-05-20 22:04:21 +0300

    wip:

--- 2025/pavyzdine/u1/main.cpp
+++ 2025/pavyzdine/u1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -10,15 +12,44 @@ struct DiskoMetejai {
     double res;
 };
 
-void skaitytiOlimpines(DiskoMetejai D[], int &n) {
+struct Pasiekimai {
+    string vardas;
+    double res;
+    int data;
+    string valstybe;
+};
+
+void skaitytiFailus(DiskoMetejai D[], Pasiekimai P[], int &n) {
     ifstream fin("Olimpines.txt");
-    
+    ifstream fin2("Pasiekimai.csv");
+    string vardas, res, data, valstybe;
+
     fin >> n;
     for (int i = 0; i < n; i++) {
         fin >> D[i].vardas >> D[i].res;
     }
 
+    for (int j = 0; j < n; j++) {
+        string eilute;
+
+        getline(fin2, eilute); // gauna visa pilna eilute
+        istringstream ss(eilute);
+
+        getline(ss, vardas, ';');
+        getline(ss, res, ';');
+        getline(ss, data, ';');
+        getline(ss, valstybe, ';');
+
+        P[j].vardas = vardas;
+        P[j].res = stod(res); // stod -> float
+        P[j].data = stoi(data); // stoi -> int
+        P[j].valstybe = valstybe;
+
+        //cout << vardas << " " << res << " " << data << " " << valstybe << endl;
+    }
+
     fin.close();
+    fin2.close();
 }
 
 void skaiciuotiVidurki(DiskoMetejai D[], int &n) {
@@ -31,25 +62,49 @@ void skaiciuotiVidurki(DiskoMetejai D[], int &n) {
     fout << "Vidutiniškai diskas skriejo " << vid / n << " m." << endl;
 }
 
-// n, cia atitinka metiko eiles nr
-char gautiMetejoKoda(DiskoMetejai D[], int n) {
-    return D[n].vardas[0];
+
+string gautiMetejoPavarde(string kodas) {
+    return kodas.substr(1); // pasalina pirma raide
+    // AGUDZIUS -> GUDZIUS
+}
+
+int gautiMetus(int metai) {
+    return metai / 10000;
+    // 20180610 -> 2018
+}
+
+
+void anksGeriausiasRez(DiskoMetejai D[], Pasiekimai P[], int &n) {
+    int anksciausiMetai = 9999;
+    int indeksas;
+
+    for (int i = 0; i < n; i++) { 
+        if (gautiMetus(P[i].data) < anksciausiMetai) {
+            anksciausiMetai = gautiMetus(P[i].data);
+            indeksas = i;
+        }
+    }
+
+    fout << "Seniausiai pasiektas geriausias rezultatas:" << endl;
+    fout << gautiMetejoPavarde(P[indeksas].vardas) << " " << P[indeksas].res << " " << anksciausiMetai << " " << P[indeksas].valstybe << endl;
 }
 
 int main() {
     DiskoMetejai D[32];
+    Pasiekimai P[32];
     int n;
 
     fout << "Disko metimo duomenų analizės rezultatai:" << endl << endl;
-    fout << "Olimpinės žaidynės:";
+    fout << "Olimpinės žaidynės:" << endl;
 
-    skaitytiOlimpines(D, n);
+    skaitytiFailus(D, P, n);
     skaiciuotiVidurki(D, n);
+    anksGeriausiasRez(D, P, n);
 
-    cout << gautiMetejoKoda(D, 2);
-    for (int i = 0; i < n; i++) {
-        cout << D[i].vardas << " " << D[i].res << endl;
-    }
+   
+    // for (int i = 0; i < n; i++) {
+    //     cout << D[i].vardas << " " << D[i].res << endl;
+    // }
 
     return 0;
 }
